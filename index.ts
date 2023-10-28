74bae2061511b32b06d706a3c4967439cfa96736
Eric Campbell <eric.robert.campbell@gmail.com>
2023-10-28 14:32:54 -0600

    feat: endpoint now handles strings and zip files
    You can now send a raw string (via the 'string' attribute on the body)
    or send a zip file (via the 'zip' attribute.
    
    Closes #4

--- index.ts
+++ index.ts
@@ -1,59 +1,17 @@
 import express, { Express, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
-const cors = require("cors");
-const { spawnSync } = require("child_process");
-const fs = require("fs");
+//@ts-expect-error
+import cors from "cors";
 import "dotenv/config";
 
 import { validateToken } from "./src/utility/authentication/functions";
+import { handleRequest } from "./src/handlers";
 
-const baseDir = "./tmp";
 const app: Express = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
-const doEverything = (fileText: string) => {
-	if (!fileText) {
-		return;
-	}
-	const uniqueRequestId = uuidv4();
-
-	const requestDir = `${baseDir}/${uniqueRequestId}`;
-	const mainFile = `${requestDir}/main.tex`;
-	fs.mkdirSync(requestDir, { recursive: true });
-	fs.writeFileSync(mainFile, fileText);
-
-	const clean = spawnSync("latexmk", ["-C"]);
-	const process = spawnSync("latexmk", [
-		mainFile,
-		"-pdf",
-		"--shell-escape",
-		`-output-directory=${requestDir}`,
-	]);
-
-	// read the file in
-	const fileData = fs.readFileSync(`${requestDir}/main.pdf`);
-	console.log(`got pdf data`, fileData);
-	let base64String;
-	try {
-		base64String = fileData.toString("base64");
-		console.log("success");
-	} catch (e) {
-		console.error("Error getting the pdf file contents");
-		console.error(e);
-		throw e;
-	}
-
-	// delete the file that you just created
-	fs.rmSync(requestDir, {
-		recursive: true,
-		force: true,
-	});
-
-	return base64String;
-};
 app.post("/", async (req: Request, res: Response) => {
 	const authorizationHeader = req.header("Authorization");
 	const isTokenValid = await validateToken(authorizationHeader);
@@ -63,11 +21,12 @@ app.post("/", async (req: Request, res: Response) => {
 		);
 		return;
 	}
-	const fileString = req.body.string;
-	console.log("body:", req.body);
-	// console.log("body", req);
-	const data = doEverything(fileString);
-	// console.log("data:", data);
+	if (!req.body.string && !req.body.zip) {
+		res.status(400).send(
+			"Unable to find a valid 'zip' or 'string' attribute in request body"
+		);
+	}
+	const data = handleRequest(req, res);
 	res.status(200).send(data);
 });
 
